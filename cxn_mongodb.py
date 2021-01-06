c8298c71d139dffcdd2cf25c0b539a748dafc5d1
Oğuzhan <ouzhan.ysl@gmail.com>
2021-01-06 15:10:49 +0300

    added mongodb

--- cxn_mongodb.py
+++ cxn_mongodb.py
@@ -1,5 +1,13 @@
+from encode_decode import ImageCoding
+import datetime
+
+class mDB:
+    def __init__(self, license_plate, cam_id):
+        self.license_plate = license_plate
+
+    def w_LP_Encode(self):
+        img_encode = ImageCoding("plaka.jpg").encodeImage()
 
 
-class MongoDB:
-    def __init__(self):
-        pass
+if __name__ == "__main__":
+    mDB("06YUH32", "34_01").w_LP_Encode()
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-import base64
-import json
-
-class ImageCoding:
-    def __init__(self, path):   # read file in path
-        with open(path,"rb") as img:
-            self.read_img = img.read()
-
-    def encodeImage(self):      # encrypt image
-        img_64_encode = base64.encodebytes(self.read_img)
-        with open("encode_plate.txt", "wb") as enimg:
-            enimg.write(img_64_encode)
-        return img_64_encode
-
-    def decodeImage(self):      # decrypt image
-        img_64_decode = base64.decodebytes(self.read_img)
-        with open("decode_plate.jpg", "wb") as deimg:
-            deimg.write(img_64_decode)
+from cxn_mongodb import mDB
 
 
 if __name__ == "__main__":
-    ImageCoding(path="plaka.jpg").encodeImage()
-    # ImageCoding(path="encode_plate.txt").decodeImage()
+    mDB("06YUH32", "34_01").w_LP_Encode()
