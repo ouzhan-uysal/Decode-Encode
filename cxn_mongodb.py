cfec6c238be4b2f0e6f740b3f04e409e5909db7e
Oğuzhan <ouzhan.ysl@gmail.com>
2021-01-08 10:56:29 +0300

    x

--- cxn_mongodb.py
+++ cxn_mongodb.py
@@ -1,13 +1,20 @@
 from encode_decode import ImageCoding
 import datetime
+from pymongo import MongoClient
 
 class mDB:
     def __init__(self, license_plate, cam_id):
         self.license_plate = license_plate
+        
+        client = MongoClient("mongodb+srv://ouz:c4A1P24cw01EZpc7@lp-encode-cluster.oydxs.mongodb.net/Previous-Parking-Spots?retryWrites=true&w=majority")
+        db = client["Previous-Parking-Spots"]
+        collection = db["license_plates"]
+
 
     def w_LP_Encode(self):
         img_encode = ImageCoding("plaka.jpg").encodeImage()
 
 
+
 if __name__ == "__main__":
-    mDB("06YUH32", "34_01").w_LP_Encode()
+    mDB("06YUH32", "34_01")
